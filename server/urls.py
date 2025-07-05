c0b0275b5ecc889e42d6b0a41707242e96118b58
anoohyakanaparthi <anoohyakanaparthi@gmail.com>
2025-07-05 12:46:25 +0530

    Update urls.py

--- server/urls.py
+++ server/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('',include('calculator.urls'))
-    #path('',include('melody.urls'))
-    path('',include('brain_tumor.urls'))
+    path('',include('melody.urls'))
+    #path('',include('brain_tumor.urls'))
 ]
